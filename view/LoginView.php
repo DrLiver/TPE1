b3294e745ffa0a02c58d0ca768e842836eb607b8
Alex Farias Dos Santos <fariasdossantosalex@gmail.com>
2021-10-18 16:15:49 -0300

    acomodando templates

--- view/LoginView.php
+++ view/LoginView.php
@@ -6,14 +6,12 @@ class LoginView{
 
     private $smarty;
     private $authHelper;
-    private $equipoView;
-
+    
     function __construct(){
         $this->smarty = new Smarty();
         $this->authHelper = new AuthHelper();
-        $this->equipoView = new EquipoView();
     }
-    
+
     public function showRegister($message = '') {
         $this->smarty->assign('SESSION', $this->authHelper->session());
         $this->smarty->assign('loginError');
